e2d6c9506d13f3b0795bd4ccee648f1f8e3e7562
jensenzhang <jingxuan.n.zhang@gmail.com>
2016-12-11 18:53:33 +0800

    Add utility script for deployment
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+WORK_DIR=$(dirname $0)
+cd $WORK_DIR
+echo 'Enter $WORK_DIR'
+
+echo 'Check themes...'
+if [ ! -d themes/hugo-theme-cooler ]; then
+    echo 'themes/hugo-theme-cooler is not found'
+    echo 'Clone themes/hugo-theme-cooler from github...'
+    git clone https://github.com/snlab-freedom/hugo-theme-cooler themes/hugo-theme-cooler
+    echo 'Done!'
+fi
+
+pushd themes/hugo-theme-cooler
+if [ -z $(git status --porcelain) ]; then
+    echo 'themes/hugo-theme-cooler is clean'
+    echo 'Sync themes/hugo-theme-cooler from github...'
+    git pull
+    echo 'Done!'
+fi
+popd
+
+echo 'Check public...'
+if [ -d public ] then;
+    echo 'public exists'
+    pushd public;
+    echo 'Check workspace status:'
+    git status --porcelain
+    if [ $? -eq 0 ]; then
+        if [ -z $(git status --porcelain) ]; then
+            echo 'Workspace is not clean'
+            echo 'Cleanup the workspace'
+            git clean -xdf
+            git reset --hard origin/master
+        fi
+        echo 'Sync public from github...'
+        git pull;
+        popd;
+    else
+        echo 'There is no git repo :('
+        popd
+        rm -rf public
+        git clone https://github.com/snlab-freedom/snlab-freedom.github.io public
+    fi
+else
+    echo 'There is no git repo :('
+    rm -rf public
+    git clone https://github.com/snlab-freedom/snlab-freedom.github.io public
+fi
+
+hugo
